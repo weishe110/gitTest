6cff000bd0179409ea521ed698c5d74449e08a91
bobo <738829323@qq.com>
2021-08-27 12:32:48 +0800

    second commit

--- src/main/java/edu/mju/gitTester.java
+++ src/main/java/edu/mju/gitTester.java
@@ -3,5 +3,6 @@ package edu.mju;
 public class gitTester {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("hello world 2");
     }
 }
